0602171beeaa1c23028935b3f680dd7476bcd243
diddy-with-baby-oil <isavyxd@outlook.com>
2024-11-25 16:23:49 -0600

    Create scrollEffect.js

---
+++ scrollEffect.js
@@ -0,0 +1,14 @@
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    },
+    { threshold: 0.1 }
+);
+
+fadeIns.forEach((fadeIn) => observer.observe(fadeIn));
